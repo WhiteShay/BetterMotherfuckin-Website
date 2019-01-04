e1d87cb7ab692d00a751f8e0a6aa6264436da8b9
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 17:31:32 +0100

    Essais settimeout

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ let popup = document.getElementById("popup");
 let closeBtn = document.getElementById("closeBtn");
 let linkBtn = document.getElementById("linkBtn");
 
+
+function Popup(){ 
+   bckgrndlayout = setTimeout(function(){document.getElementById("bckgrndlayout").style.display="block";},10000
+     ;
+  }
+
 linkBtn.addEventListener('click',openPage);
   function openPage() {
       document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
