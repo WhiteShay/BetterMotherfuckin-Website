ae80d1f4a2267837cb526dc75a53e11d5975c95c
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 05:16:09 +0100

    Ajout display fonction

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 function displayMessage() {
-    var html = document.querySelector('html');
+    let html = document.querySelector('html');
 
-    var panel = document.createElement('div');
+    let panel = document.createElement('div');
     panel.setAttribute('class', 'msgBox');
     html.appendChild(panel);
     
-    var msg = document.createElement('p');
+    let msg = document.createElement('p');
     msg.textContent = 'Hey le site xxxvidsxxx est trop bien. Viens dessus stp please';
     panel.appendChild(msg);
     
-    var closeBtn = document.createElement('button');
+    let closeBtn = document.createElement('button');
     closeBtn.textContent = 'Non merci';
     panel.appendChild(closeBtn);
 
-    var linkBtn = document.createElement('button');
+    let linkBtn = document.createElement('button');
     linkBtn.textContent = 'OK'
     panel.appendChild(linkBtn)
     
     closeBtn.onclick = function() {
       panel.parentNode.removeChild(panel);
     }
-}
+  }
+  displayMessage();
