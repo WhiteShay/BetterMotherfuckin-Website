6e5c051e14c10258bd185e7ea146b0a0ed24f1d2
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 19:09:21 +0100

    Retour à la version stable, abandon de settimeout.

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
 
-setTimeout(popUp, 10000);
-
-function popUp(){
-  
     let bckgrndlayout = document.getElementById("bckgrndlayout");
     let popup = document.getElementById("popup");
     let closeBtn = document.getElementById("closeBtn");
     let linkBtn = document.getElementById("linkBtn");
-
-    result = popup.confirm("Salut mon pote ça va ? tu veux faire un tour sur notre site ? ");
-
-  if(result ==false){
-    while(result==false){
-        result = popup.confirm("Salut mon pote ça va ? tu veux faire un tour sur notre site ? ");
-    }
-    popup.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    }
-    else{
-    popup.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    }
 
 linkBtn.addEventListener('click',openPage);
   function openPage() {
@@ -29,5 +13,3 @@ btnClose.addEventListener('click',closePopup);
   function closePopup() {
     bckgrndlayout.style.display = "none";
   }
-
-}
