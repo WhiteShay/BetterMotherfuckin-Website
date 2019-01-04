bec330ac800856a2f884ea9785444219094bd616
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 18:51:51 +0100

    Essais setTimeout avec If/while/else (non fonctionelle)

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 
+setTimeout(popUp, 10000);
 
-let bckgrndlayout = document.getElementById("bckgrndlayout");
-let popup = document.getElementById("popup");
-let closeBtn = document.getElementById("closeBtn");
-let linkBtn = document.getElementById("linkBtn");
+function popUp(){
+  
+    let bckgrndlayout = document.getElementById("bckgrndlayout");
+    let popup = document.getElementById("popup");
+    let closeBtn = document.getElementById("closeBtn");
+    let linkBtn = document.getElementById("linkBtn");
 
+    result = popup.confirm("Salut mon pote ça va ? tu veux faire un tour sur notre site ? ");
 
-function Popup(){ 
-   bckgrndlayout = setTimeout(function(){document.getElementById("bckgrndlayout").style.display="block";},10000
-}
+  if(result ==false){
+    while(result==false){
+        result = popup.confirm("Salut mon pote ça va ? tu veux faire un tour sur notre site ? ");
+    }
+    popup.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    }
+    else{
+    popup.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    }
 
 linkBtn.addEventListener('click',openPage);
   function openPage() {
@@ -19,3 +29,5 @@ btnClose.addEventListener('click',closePopup);
   function closePopup() {
     bckgrndlayout.style.display = "none";
   }
+
+}
