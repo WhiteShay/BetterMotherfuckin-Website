f702ac3ead6feb97d8842fd78b65dbeecccaf5d9
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 06:37:52 +0100

    Modification  de  'class' en 'id' pour msgBox , ajout fonction linkBtn

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ function displayMessage() {
     let html = document.querySelector('html');
 
     let panel = document.createElement('div');
-    panel.setAttribute('class', 'msgBox');
+    panel.setAttribute('id','msgBox');
     html.appendChild(panel);
     
     let msg = document.createElement('p');
     msg.textContent = 'Hey le site xxxvidsxxx est trop bien. Viens dessus stp please';
     panel.appendChild(msg);
     
-    let closeBtn = document.createElement('button');
+    let closeBtn = document.createElement('button_1');
     closeBtn.textContent = 'Non merci';
     panel.appendChild(closeBtn);
 
-    let linkBtn = document.createElement('button');
+    let linkBtn = document.createElement('button_2');
     linkBtn.textContent = 'OK'
     panel.appendChild(linkBtn)
     
     closeBtn.onclick = function() {
       panel.parentNode.removeChild(panel);
+    }
+    
+    linkBtn.onclick = function() {
+
     }
   }
 setTimeout(displayMessage,10000);
