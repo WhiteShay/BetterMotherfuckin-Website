8e9d38f44311a370e5e1c8db9248bde0e61b0022
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 13:43:48 +0100

    Modifications importantes, changement de méthode

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-function displayMessage() {
-    let html = document.querySelector('html');
+var bckgrndlayout = document.getElementById("bckgrndlayout");
+var popup = document.getElementById("popup");
+var closeBtn = document.getElementById("closeBtn");
+var linkBtn = document.getElementById("linkBtn");
 
-    let panel = document.createElement('div');
-    panel.setAttribute('id','msgBox');
-    html.appendChild(panel);
-    
-    let msg = document.createElement('p');
-    msg.textContent = 'Hey le site xxxvidsxxx est trop bien. Viens dessus stp please';
-    panel.appendChild(msg);
-    
-    let closeBtn = document.createElement('closeBtn');
-    panel.setAttribute('id','closeBtn')
-    closeBtn.textContent = 'Non merci';
-    panel.appendChild(closeBtn);
-
-    let linkBtn = document.createElement('linkBtn');
-    panel.setAttribute('id','linkBtn')
-    linkBtn.textContent = 'OK'
-    panel.appendChild(linkBtn)
-    
-    closeBtn.onclick = function() {
-      panel.parentNode.removeChild(panel);
-    }
-    
-    linkBtn.onclick = function() {
-
-    }
-  }
-setTimeout(displayMessage,10000);
+var bckgrndlayout = setTimeout(function(),10000)
